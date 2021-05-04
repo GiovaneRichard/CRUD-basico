f6c883bb1ba75b896ac47916b521efd81148eb35
Giovane Richard <giovanerichard@gmail.com>
2021-05-04 09:33:44 -0300

    testes de hospedagem

--- config.php
+++ config.php
@@ -3,14 +3,19 @@
 	$email_adm = 'giovanerichard@gmail.com';
 	$url_site = 'http://localhost/crud_basico';
 
-	// DADOS PARA CONEXÃO
+/*
+	// DADOS PARA CONEXÃO LOCAL
 	$banco = 'ecommerce';
 	$host = 'localhost';
 	$usuario = 'root';
 	$senha = '';
+*/
 
-	// TOKEN DE ACESSO
-	$token = '123';
+	// DADOS PARA CONEXÃO NUVEM
+	$banco = 'd4i69q99i2fkmk';
+	$host = 'ec2-18-214-195-34.compute-1.amazonaws.com';
+	$usuario = 'shvmqczpqhawyq';
+	$senha = '475a3006ca4839843ca95f547df33591d52a62ce94bfce40b144cfecb2db566a';
 
 
 //CONFIGURAÇÕES PARA PAGINAÇÃO DE ITENS NO PAINEL
@@ -22,5 +27,7 @@ $itens_por_pagina = 5;
 $itens_por_pagina_1 = 5;
 $itens_por_pagina_2 = 10;
 $itens_por_pagina_3 = 15;
+
+
 
  ?>
