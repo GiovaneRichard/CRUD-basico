c3d94c482ca1976b0de1b50f5adadf61de76107c
Giovane Richard <giovanerichard@gmail.com>
2021-05-04 09:50:34 -0300

    alterações 02

--- conexao.php
+++ conexao.php
@@ -7,7 +7,7 @@ date_default_timezone_set('America/Sao_Paulo');
 try {
 	//$pdo = new PDO("mysql:dbname=$banco;host=$host;charset=utf8", "$usuario", "$senha");
 	
-	$pdo = new PDO("pgsql:host=$host;port=;dbname=$banco";"$usuario", "$senha");
+	$pdo = new PDO("pgsql:host=$host;dbname=$banco";"$usuario", "$senha");
 ?>
 
 } catch (Exception $e) {
